8304efaf17d5d1432ba99addaffd7f3f325eea7e
Prerit Dayal <prerit714@gmail.com>
2026-01-07 12:48:40 -0500

    Template for c++ files

--- lua/cp/init.lua
+++ lua/cp/init.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+M.__handle_cpp = function()
+  -- Get the directory where init.lua is located
+  local source = debug.getinfo(1, "S").source:sub(2)
+  local plugin_dir = vim.fn.fnamemodify(source, ":h")
+  local template_path = plugin_dir .. "/solution.cpp"
+
+  -- Read the template file
+  local template_lines = vim.fn.readfile(template_path)
+  -- Replace the current buffer content with template
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, template_lines)
+
+  -- Move cursor to specific position
+  vim.api.nvim_win_set_cursor(0, { 64, 0 })
+end
+
 --- Handle java template injection
 M.__handle_java = function()
   -- Get the directory where init.lua is located
@@ -24,6 +39,10 @@ M.setup = function()
 
     if file_type == "java" then
       M.__handle_java()
+    end
+
+    if file_type == "cpp" then
+      M.__handle_cpp()
     end
   end, {
     desc = "Populate current buffer with CP template",
---
+++ lua/cp/solution.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define SZ(x) ((int)(x).size())
+#define SQ(x) ((x) * (x))
+#define CU(x) ((x) * (x) * (x))
+
+#define DEB(x) cout << #x << " = " << x << endl
+#define DEB2(x, y) cout << #x << " = " << x << ", " << #y << " = " << y << endl
+
+#define DEBARR(arr, n)                                                         \
+  cout << #arr << " : ";                                                       \
+  for (int i = 0; i < n; i++)                                                  \
+    cout << arr[i] << " ";                                                     \
+  cout << endl
+
+#define DEBVEC(v)                                                              \
+  cout << #v << " : ";                                                         \
+  for (auto x : v)                                                             \
+    cout << x << " ";                                                          \
+  cout << endl
+
+#define DEBMAP(m)                                                              \
+  cout << #m << " : ";                                                         \
+  for (auto &[k, v] : m)                                                       \
+    cout << "{" << k << " -> " << v << "} ";                                   \
+  cout << endl
+
+#define FASTIO                                                                 \
+  ios::sync_with_stdio(0);                                                     \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+
+#define REP(i, x, y) for (int i = (x); i <= (y); ++i)
+
+#define REPR(i, x, y) for (int i = (x); i >= (y); --i)
+
+struct Timer {
+  chrono::high_resolution_clock::time_point start;
+  Timer() { start = chrono::high_resolution_clock::now(); }
+  ~Timer() {
+    auto end = chrono::high_resolution_clock::now();
+    cerr << "[TIME]: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms\n";
+  }
+};
+
+#define timed(x)                                                               \
+  {                                                                            \
+    Timer __t;                                                                 \
+    x;                                                                         \
+  }
+
+void solve() {
+  // Author: https://prerit714.cloud
+}
+
+int main() {
+  FASTIO;
+  timed({ solve(); });
+  return EXIT_SUCCESS;
+}
